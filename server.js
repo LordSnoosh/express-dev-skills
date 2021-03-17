be3c889d29f0ec72be131cb4ec52a301a9597ad5
github_username <lordsnoosh@gmail.com>
2021-03-16 17:18:52 -0700

    contacts test

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/skills', skillsRouter);
+app.use('/contacts', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
