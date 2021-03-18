86ee0a19c9c4b946db8b8221d6f73cbabf5f45e3
github_username <lordsnoosh@gmail.com>
2021-03-17 22:04:53 -0700

    part 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,16 +1,38 @@
-const Skill = require('../models/skills');
-
+const Skill = require('../models/skill');
 module.exports = {
   index,
-  show
+  show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update
 };
-
+function update(req, res) {
+  req.body.done = !!req.body.done;
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
+}
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', { skill });
+}
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect('/skills');
+}
+function newSkill(req, res) {
+  res.render('skills/new');
+}
 function show(req, res) {
   res.render('skills/show', {
     skill: Skill.getOne(req.params.id)
   });
 }
-
 function index(req, res) {
   res.render('skills/index', {
     skills: Skill.getAll()
--- models/skills.js
+++ models/skills.js
@@ -1,20 +1,40 @@
 const skills = [
-    {id: 125223, skill: 'JavaScript', done: true},
-    {id: 127904, skill: 'HTML', done: true},
-    {id: 139608, skill: 'Node', done: true},
-    {id: 221430, skill: 'CSS', done: true}
-  ];
-  
-  module.exports = {
-    getAll,
-    getOne
-  };
-  
-  function getOne(id) {
-    id = parseInt(id);
-    return skills.find(skill => skill.id === id);
-  }
-  
-  function getAll() {
-    return skills;
-  }
+  {id: 125223, skill: 'JavaScript', done: true},
+  {id: 127904, skill: 'HTML', done: true},
+  {id: 139608, skill: 'Node', done: true},
+  {id: 221430, skill: 'CSS', done: true}
+];
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  deleteOne,
+  update
+};
+function update(id, skill) {
+  id = parseInt(id);
+  const idx = skills.findIndex(skill => skill.id === id);
+  skill.id = id;
+  skills.splice(idx, 1, skill);
+}
+function deleteOne(id) {
+  id = parseInt(id);
+  // skills = skills.filter(t => t.id !== id);
+  // Another approach
+  const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
+}
+function create(skill) {
+  // Add the id
+  skill.id = Date.now() % 1000000;
+  // New skills should not be "done"
+  skill.done = false;
+  skills.push(skill);
+}
+function getOne(id) {
+  id = parseInt(id);
+  return skills.find(skill => skill.id === id);
+}
+function getAll() {
+  return skills;
+}
